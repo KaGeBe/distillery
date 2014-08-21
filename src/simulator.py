3975a07eb83af22e26e10d8e1f20d73d5686c9f3
Klaas Boesche <boesche@cs.uni-saarland.de>
2014-08-21 23:06:06 +0200

    refactor checking of invalid moves such that it can be used without exceptions in strategies

--- src/simulator.py
+++ src/simulator.py
@@ -16,15 +16,41 @@ class GameLogicError(Exception):
 
 class MoveError(GameLogicError):
 
-    def __init__(self, x0, y0, x1, y1, count=None, value=None):
+    def __init__(self, x0, y0, x1, y1, count=None, cause=None, cause_args=None):
         self.src = (x0, y0)
         self.tgt = (x1, y1)
         self.count = count
-        self.value = value
+        self.cause = cause
+        self.cause_args = cause_args
 
     def __str__(self):
-        return "Cannot move {} piece(s) from {} to {}: {}" \
-               .format(self.count, self.src, self.tgt, self.value)
+        return "Cannot move {} piece(s) from {} to {}: " \
+               .format(self.count, self.src, self.tgt) \
+               + self.cause().format(**self.cause_args)
+
+
+class MoveErrorCause:
+
+    def origin_tower_too_small():
+        return "Not enough tower pieces at origin."
+
+    def no_target_tower():
+        return "Target place does not have a tower."
+
+    def invalid_direction():
+        return "Invalid direction"
+
+    def origin_equals_target():
+        return "Origin is equal target"
+
+    def distance_too_large():
+        return "Distance {distance} too large to target of height {height}"
+
+    def blocking_tower():
+        return "Blocking tower in between at ({x}, {y})"
+
+    def is_undo():
+        return "Would undo the last move."
 
 
 class Simulator:
@@ -58,29 +84,23 @@ class Simulator:
     def place(self, x, y, color):
         print("Placing {} on {}.".format(color, (x, y)))
         place = self.field[x][y]
-        if not len(place) == 0:
+        if not self.can_place(x, y):
             raise GameLogicError("Not an empty field.")
 
         place.append(color)
         return False
 
+    def can_place(self, x, y):
+        return len(self.field[x][y]) == 0
+
     def move(self, x0, y0, x1, y1, count):
         print("Moving {} piece(s) from {} to {}."
               .format(count, (x0, y0), (x1, y1)))
-        self.check_undo(x0, y0, x1, y1, count)
+        allowed, error_cause, cause_args = self.can_move(x0, y0, x1, y1, count)
+        if not allowed:
+            raise MoveError(x0, y0, x1, y1, count, error_cause, cause_args)
 
         place = self.field[x0][y0]
-        if len(place) < count:
-            raise MoveError(x0, y0, x1, y1, count,
-                            "Not enough tower pieces at origin.")
-
-        target_place = self.field[x1][y1]
-        if len(target_place) == 0:
-            raise MoveError(x0, y0, x1, y1, count,
-                            "Target place does not have a tower.")
-
-        self.check_sight(x0, y0, x1, y1, count)
-
         pieces = place[-count:]
         self.field[x1][y1].extend(pieces)
         del place[-count:]
@@ -92,44 +112,48 @@ class Simulator:
             return (place[-1], x, y)
         return False
 
-    def check_undo(self, x0, y0, x1, y1, count):
-        if self.last_action == (x1, y1, x0, y0, count):
-            raise MoveError(x0, y0, x1, y1, count, "Would undo the last move.")
+    def is_undo(self, x0, y0, x1, y1, count):
+        return self.last_action == (x1, y1, x0, y0, count)
 
-    def check_sight(self, x0, y0, x1, y1, count):
+    def can_move(self, x0, y0, x1, y1, count):
+        if self.is_undo(x0, y0, x1, y1, count):
+            return (False, MoveErrorCause.is_undo, {})
+        if len(self.field[x0][y0]) < count:
+            return (False, MoveErrorCause.origin_tower_too_small, {})
         target_height = len(self.field[x1][y1])
+        if target_height == 0:
+            return (False, MoveErrorCause.no_target_tower, {})
         dist_x = abs(x1 - x0)
         dist_y = abs(y1 - y0)
         dir_x = compare(x1, x0)
         dir_y = compare(y1, y0)
         if dist_x == 0 and dist_y == 0:
-            raise MoveError(x0, y0, x1, y1, count, "Origin is equal target")
+            return (False, MoveErrorCause.origin_equals_target, {})
         elif dist_x == dist_y or dist_x == 0 or dist_y == 0:
             dist = max([dist_x, dist_y])
             if dist > target_height:
-                raise MoveError(x0, y0, x1, y1, count,
-                                "Distance {} too far from target tower of"
-                                " height {}.".format(dist, target_height))
-            # now checking that no tower between both fields
+                return (False, MoveErrorCause.distance_too_large,
+                        {"distance": dist, "height": target_height})
+            # now check that no tower between both fields
             for i in range(1, dist):
                 x = x0 + (dir_x * i)
                 y = y0 + (dir_y * i)
                 place = self.field[x][y]
                 if len(place) > 0:
-                    raise MoveError(x0, y0, x1, y1, count,
-                                    "Blocking tower in between at ({}, {})"
-                                    .format(x, y))
-
+                    return (False, MoveErrorCause.blocking_tower,
+                            {"x": x, "y": y})
+            return (True, None, None)
         else:
-            raise MoveError(x0, y0, x1, y1, count, "Not in a valid direction.")
+            return (False, MoveErrorCause.invalid_direction)
 
     def __str__(self):
-        return "\n".join([" ".join([str(place) for place in row])
+        return "\n".join([" ".join(["[{}]".format("".join(place))
+                                   for place in row])
                          for row in self.field]) + "\n"
 
 
 if __name__ == "__main__":
-    sim = Simulator()
+    sim = Simulator(None, None)
     sim.place(3, 3, "R")
     sim.place(2, 2, "R")
     sim.move(3, 3, 2, 2, 1)
